9acb883e6cd12e5dfdd8189e48c854e0faa052ab
Kevin <kees.vincent.ajunan@gmail.com.com>
2026-09-04 23:04:02 +0800

    [AJUNAKE] add h2 database and JPA Repository and implement in User model

--- src/main/java/com/project/EComApplication/users/User.java
+++ src/main/java/com/project/EComApplication/users/User.java
@@ -1,15 +1,23 @@
 package com.project.EComApplication.users;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity(name="user_table")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
